c7349388c7389fa3db79deed8d4a94d09ad8bdbe
bpazich <Vfqrhjcjangfhjkm666>
2019-05-05 21:52:30 +0300

    fix re call setSource minor improve

--- container-tree.ts
+++ container-tree.ts
@@ -3,10 +3,10 @@ import {File} from "./src/file";
 import {Explorer} from "./src/explorer";
 
 export interface ISource {
-	containerType: "directory" | "file"
-	children?: Array<ISource>;
+    containerType: "directory" | "file"
+    children?: Array<ISource>;
 
-	[k: string]: any;
+    [k: string]: any;
 }
 
 type TTemplate = (source: ISource, directory?: Directory) => HTMLDivElement;
@@ -15,66 +15,69 @@ type TTemplate = (source: ISource, directory?: Directory) => HTMLDivElement;
  * @desc Create tree structure from source object.
  * */
 export class ContainerTree extends Explorer {
-	public root: Directory;
-	public currentDir: Directory;
-	private _sources: Array<ISource>;
-	private _fileTemplate: TTemplate;
-	private _folderTemplate: TTemplate;
+    public root: Directory;
+    public currentDir: Directory;
+    private _sources: Array<ISource>;
+    private _containers: Array<Directory | File>;
+    private _fileTemplate: TTemplate;
+    private _folderTemplate: TTemplate;
 
-	constructor() {
-		super();
-		this._sources = [];
-		this.root = new Directory();
-		this.root.open();
-	}
+    constructor() {
+        super();
+        this._sources = [];
+        this._containers = [];
+        this.root = new Directory();
+        this.root.open();
+    }
 
-	/**@desc Set source*/
-	public setSource(sources: Array<ISource>) {
-		this._sources = this._sources.concat(sources);
-		this._buildTree(this._sources, this.root);
-		return this;
-	}
+    /**@desc Set source*/
+    public setSource(sources: Array<ISource>) {
+        this._sources = this._sources.concat(sources);
+        this._buildTree(sources, this.root);
+        return this;
+    }
 
-	/** @desc Set folder template for define custom folder ui and clickToOpen element */
-	public setFolderTemplate(template: TTemplate) {
-		this._folderTemplate = template;
-		return this;
-	}
+    /** @desc Set folder template for define custom folder ui and clickToOpen element */
+    public setFolderTemplate(template: TTemplate) {
+        this._folderTemplate = template;
+        return this;
+    }
 
-	/** @desc Define file custom element*/
-	public setFileTemplate(template: TTemplate) {
-		this._fileTemplate = template;
-		return this;
-	}
+    /** @desc Define file custom element*/
+    public setFileTemplate(template: TTemplate) {
+        this._fileTemplate = template;
+        return this;
+    }
 
-	/** @desc Build tree by source */
-	private _buildTree(sources: Array<ISource>, parent: Directory, nesting = 1) {
-		sources.forEach(source => {
-			let _item;
-			if (source.containerType === "directory") {
-				_item = new Directory();
-				_item.setNestingLevel(nesting);
-				_item.parentDirectory = parent;
-				_item.onStateChange((state: EState, dir: Directory) => {
-					if (state === EState.open)
-						this.currentDir = dir;
-				});
-				if (this._folderTemplate) {
-					const _face = this._folderTemplate(source, _item);
-					_item.faceWrapper.appendChild(_face);
-				}
-				if (source.children)
-					this._buildTree(source.children, _item, (nesting + 1));
-			} else {
-				_item = new File();
-				if (this._fileTemplate) {
-					const _file = this._fileTemplate(source);
-					_item.container.appendChild(_file);
-				}
-			}
-			parent.setItem(_item);
-		});
-	}
+    /** @desc Build tree by source */
+    private _buildTree(sources: Array<ISource>, parent: Directory, nesting = 1) {
+        sources.forEach(source => {
+            let _item;
+            if (source.containerType === "directory") {
+                _item = new Directory();
+                _item.setNestingLevel(nesting);
+                _item.parentDirectory = parent;
+                _item.onStateChange((state: EState, dir: Directory) => {
+                    if (state === EState.open)
+                        this.currentDir = dir;
+                });
+                if (this._folderTemplate) {
+                    const _face = this._folderTemplate(source, _item);
+                    _item.faceWrapper.appendChild(_face);
+                }
+                if (source.children)
+                    this._buildTree(source.children, _item, (nesting + 1));
+            } else {
+                _item = new File();
+                if (this._fileTemplate) {
+                    const _file = this._fileTemplate(source);
+                    _item.container.appendChild(_file);
+                }
+            }
+            parent.setItem(_item);
+            this._containers.push(_item);
+        });
+    }
 }
 
 (window as any).ContainerTree = ContainerTree;
--- src/directory.ts
+++ src/directory.ts
@@ -1,89 +1,90 @@
 import {File} from "./file";
 
 export enum EState {
-	open,
-	close
+    open,
+    close
 }
 
 export class Directory {
-	public container: HTMLDivElement;
-	public faceWrapper: HTMLDivElement;
-	public itemsContainer: HTMLDivElement;
-	public items: Array<File | Directory> = [];
-	public parentDirectory: Directory;
-	public nestingLevel: number;
-	public state;
-	private clickToOpenElement: HTMLElement;
-	private _stateChangeHandler: (state: EState, dir: Directory) => void = (state) => {
-	};
-	private clickHandler = () => this.open();
+    public container: HTMLDivElement;
+    public faceWrapper: HTMLDivElement;
+    public itemsContainer: HTMLDivElement;
+    public items: Array<File | Directory> = [];
+    public parentDirectory: Directory;
+    public nestingLevel: number;
+    public state;
+    private clickToOpenElement: HTMLElement;
+    private _stateChangeHandler: (state: EState, dir: Directory) => void = (state) => {
+    };
+    private clickHandler = () => this.open();
 
-	constructor() {
-		this._createNode();
-		this.setClickToOpenElement(this.faceWrapper);
-		this.container
-			.appendChild(this.faceWrapper);
-		this.container
-			.appendChild(this.itemsContainer);
-	}
+    constructor() {
+        this._createNode();
+        this.setClickToOpenElement(this.faceWrapper);
+        this.container
+            .appendChild(this.faceWrapper);
+        this.container
+            .appendChild(this.itemsContainer);
+    }
 
-	public open(): Directory {
-		this.state = EState.open;
-		this.container.setAttribute("open", "");
-		this.faceWrapper.style.display = "none";
-		this.itemsContainer.style.display = "";
-		if (this.parentDirectory)
-			this.parentDirectory.items.forEach(item => {
-				item.container.style.display = item != this ? "none" : "";
-			});
-		this._stateChangeHandler(EState.open, this);
-		return this;
-	}
+    public open(): Directory {
+        this.state = EState.open;
+        this.container.setAttribute("open", "");
+        this.faceWrapper.style.display = "none";
+        this.itemsContainer.style.display = "";
+        this.itemsContainer.style.overflow = "scroll";
+        if (this.parentDirectory)
+            this.parentDirectory.items.forEach(item => {
+                item.container.style.display = item != this ? "none" : "";
+            });
+        this._stateChangeHandler(EState.open, this);
+        return this;
+    }
 
-	public close(): Directory {
-		this.state = EState.close;
-		this.faceWrapper.style.display = "";
-		this.itemsContainer.style.display = "none";
-		this.container.removeAttribute("open");
-		if (this.parentDirectory)
-			this.parentDirectory.items.forEach(item =>
-				item.container.style.display = "");
-		this._stateChangeHandler(EState.close, this);
-		return this;
-	}
+    public close(): Directory {
+        this.state = EState.close;
+        this.faceWrapper.style.display = "";
+        this.itemsContainer.style.display = "none";
+        this.container.removeAttribute("open");
+        if (this.parentDirectory)
+            this.parentDirectory.items.forEach(item =>
+                item.container.style.display = "");
+        this._stateChangeHandler(EState.close, this);
+        return this;
+    }
 
-	public setNestingLevel(level): Directory {
-		this.nestingLevel = level;
-		this.container.setAttribute("nesting-level", level);
-		return this;
-	}
+    public setNestingLevel(level): Directory {
+        this.nestingLevel = level;
+        this.container.setAttribute("nesting-level", level);
+        return this;
+    }
 
-	public setItem(item: File | Directory) {
-		this.items.push(item);
-		this.itemsContainer.appendChild(item.container);
-		return this;
-	}
+    public setItem(item: File | Directory) {
+        this.items.push(item);
+        this.itemsContainer.appendChild(item.container);
+        return this;
+    }
 
-	public setClickToOpenElement(element: HTMLElement): Directory {
-		if (this.clickToOpenElement)
-			this.clickToOpenElement.removeEventListener("click", this.clickHandler);
-		this.clickToOpenElement = element;
-		this.clickToOpenElement.addEventListener("click", this.clickHandler);
-		return this;
-	}
+    public setClickToOpenElement(element: HTMLElement): Directory {
+        if (this.clickToOpenElement)
+            this.clickToOpenElement.removeEventListener("click", this.clickHandler);
+        this.clickToOpenElement = element;
+        this.clickToOpenElement.addEventListener("click", this.clickHandler);
+        return this;
+    }
 
-	public onStateChange(cb: (state: EState, dir) => void): Directory {
-		this._stateChangeHandler = cb;
-		return this;
-	}
+    public onStateChange(cb: (state: EState, dir) => void): Directory {
+        this._stateChangeHandler = cb;
+        return this;
+    }
 
-	private _createNode() {
-		this.container = document.createElement("div");
-		this.container.setAttribute("is-folder", "true");
-		this.faceWrapper = document.createElement("div");
-		this.faceWrapper.setAttribute("dir-face", "");
-		this.itemsContainer = document.createElement("div");
-		this.itemsContainer.setAttribute("items-container", "");
-		this.itemsContainer.style.display = "none";
-	}
+    private _createNode() {
+        this.container = document.createElement("div");
+        this.container.setAttribute("is-folder", "true");
+        this.faceWrapper = document.createElement("div");
+        this.faceWrapper.setAttribute("dir-face", "");
+        this.itemsContainer = document.createElement("div");
+        this.itemsContainer.setAttribute("items-container", "");
+        this.itemsContainer.style.display = "none";
+    }
 }
